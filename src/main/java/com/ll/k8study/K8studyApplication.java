2e6739572862bb2374097aa04e3f3e164a1b8c66
ksuju <139477852+ksuju@users.noreply.github.com>
2025-01-18 10:10:13 +0900

    Initial commit

---
+++ src/main/java/com/ll/k8study/K8studyApplication.java
@@ -0,0 +1,13 @@
+package com.ll.k8study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8studyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8studyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/k8study/K8studyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.k8study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8studyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
